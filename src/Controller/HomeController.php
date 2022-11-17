d2af7d719a8bb4ab1648c50c24969b60e9dfe532
antony <antonypinel@gmail.com>
2022-11-17 13:55:57 +0100

    add function updateUser

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Service\UserService;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Serializer\Serializer;
 use Symfony\Component\HttpFoundation\Response;
@@ -25,16 +26,27 @@ class HomeController extends AbstractController
         return new Response($this->serialize->serialize($users, 'json'));
     }
 
-    #[Route('/home/{slug}', name: 'app_user_detail')]
-    public function showUser($slug): Response {
-        $users = $this->userService->findByName($slug);
+    #[Route('/home/{name}', name: 'app_user_detail')]
+    public function showUser($name): Response {
+        $users = $this->userService->findByName($name);
 
         return new Response(($this->serialize->serialize($users, 'json')));
     }
 
-    #[Route('/home/id/{slug}', name: 'app_user_id')]
-    public function showUserById($slug): Response {
-        $users = $this->userService->findById($slug);
+    #[Route('/home/id/{id}', name: 'app_user_id')]
+    public function showUserById($id): Response {
+        $users = $this->userService->findById($id);
+
+        return new Response(($this->serialize->serialize($users, 'json')));
+    }
+
+    #[Route('/home/edit/{id}', name: 'user_edit')]
+    public function updateUser(ManagerRegistry $doctrine, $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $users = $this->userService->findById($id);
+        $users->setName('Vincent');
+        $entityManager->flush();
 
         return new Response(($this->serialize->serialize($users, 'json')));
     }
